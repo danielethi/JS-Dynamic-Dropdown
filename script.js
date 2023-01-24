9f14ff6760e720dfc8a05a1df1f04476fc9f3ba2
danielethi <danielethi@outlook.com>
2023-01-23 22:52:07 -0500

    doropdown-commits

--- script.js
+++ script.js
@@ -12,7 +12,7 @@ let d = document.createElement("div");
 let s1 = document.createElement("select");
 let s2 = document.createElement("select");
 
-function makeDropdown(input) {
+const makeDropdown = (input) => {
   for (let val of Object.keys(input)) {
     const o1 = document.createElement("option");
     const o2 = document.createElement("option");
@@ -21,19 +21,17 @@ function makeDropdown(input) {
     s1.appendChild(o1);
     s2.appendChild(o2);
   }
-
   function changeHandler(event) {
     const index = event.target.selectedIndex;
     s1.selectedIndex = index;
     s2.selectedIndex = index;
   }
-
   s1.onchange = changeHandler;
   s2.onchange = changeHandler;
 
   d.appendChild(s1);
   d.appendChild(s2);
   document.body.appendChild(d);
-}
+};
 
 makeDropdown(state);
