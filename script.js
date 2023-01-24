ab603e3286d6f99444d3eac374f0c030030afa24
danielethi <danielethi@outlook.com>
2023-01-23 22:28:19 -0500

    dorpdown-commits

---
+++ script.js
@@ -0,0 +1,40 @@
+const state = {
+  a: "ana",
+  b: "bob",
+  c: "cinema",
+  d: "dog",
+  e: "elliot",
+  f: "fidelity",
+  g: "golang",
+};
+
+
+let d = document.createElement("div");
+let s1 = document.createElement("select");
+let s2 = document.createElement("select");
+
+function makeDropdown(input) {
+  for (val of Object.keys(input)) {
+    const o1 = document.createElement("option");
+    const o2 = document.createElement("option");
+    o1.textContent = val;
+    o2.textContent = input[val];
+    s1.appendChild(o1);
+    s2.appendChild(o2);
+  }
+
+  function changeHandler(event) {
+    const selectionIndex = event.target.selectedIndex;
+    s1.selectedIndex = selectionIndex;
+    s2.selectedIndex = selectionIndex;
+  }
+
+  s1.onchange = changeHandler;
+  s2.onchange = changeHandler;
+
+  d.appendChild(s1);
+  d.appendChild(s2);
+  document.body.appendChild(d);
+}
+
+makeDropdown(state);
